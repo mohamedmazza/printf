39709732a9ef629c3d73f5f92323f9e0b3b179fd
eskylii <essaidisouad20@gmail.com>
2023-08-22 09:38:15 -0700

    get_flags

--- get_flags.c
+++ get_flags.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
-/*
- * get_flags - calcule active flag
+/**
+ * get_flags - Calculates active flags
  * @format: formatted string to print argument
  * @i: take parameter
  * Return: Flags
- * */
+ */
 int get_flags(const char *format, int *i)
 {
 	int e, cur_i;
 	int flag = 0;
-	const char FLAGS_CH[]={'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[]= {F_MINUS, F_PLUS, F_ZERO, F_HASH,F_SPACE, 0};
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (cur_i = *i + 1; format[cur_i] != '\0'; cur_i++)
 	{
-		for (e = 0; FLAGS_CH[e] != '\0';e++)
-			if(format[cur_i] == FLAGS_CH[e])
+		for (e = 0; FLAGS_CH[e] != '\0'; e++)
+			if (format[cur_i] == FLAGS_CH[e])
 			{
 				flag |= FLAGS_ARR[e];
 				break;
---
+++ get_size.c
@@ -0,0 +1,5 @@
+#include "main.h"
+/*
+ * get_size - calcul the size to cast argument
+ * @format : formatted string in which to print the arguments
+ * @i: List of arguments to be printed
